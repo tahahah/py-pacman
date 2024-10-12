ef46bdefbb8ca1c722245eda6c9c9a2ce7745ecb
Taha Ansari <129813209+tahahah@users.noreply.github.com>
2024-10-12 13:16:26 +1100

    fix: worker restart and pika error handling

--- worker/worker.py
+++ worker/worker.py
@@ -85,7 +85,7 @@ def save_to_hf_dataset(episodes_data, episode_keys):
         # Create dataset with image column
         dataset = Dataset.from_dict(batch_dict)
         logging.info(f"Dataset size in MB: {dataset.data.nbytes / (1024 * 1024)}")
-        dataset.push_to_hub('PacmanDataset_Redis_Try', split='train', token=HF_TOKEN, data_dir=f"data/{episode_keys[0]}_{episode_keys[-1]}")
+        dataset.push_to_hub('PacmanDataset_3', split='train', token=HF_TOKEN, data_dir=f"data/{episode_keys[0]}_{episode_keys[-1]}")
         logger.info("\tSaved to Hugging Face dataset")
         logging.info("***********************************************************")
 
@@ -158,7 +158,9 @@ def main():
         'rabbitmq-host',
         5672,
         '/',
-        credentials
+        credentials,
+        connection_attempts=10,
+        retry_delay=5
     )
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
