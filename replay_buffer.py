19980bf132714603e456608e22392d09fd74c9ce
Taha Ansari <tahaaansari@hotmail.com>
2024-12-29 16:58:24 +1100

    fix: add memory cleanups to avoid leaks

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,47 +1,11 @@
 from collections import deque
 import torch
 import random
+import numpy as np
+import gc
 
 USE_CUDA = torch.cuda.is_available()
-
-
-# class ReplayBuffer(object):
-#     def __init__(self, batch_size=32):
-#         self.memory = deque(maxlen=10000)
-#         self.batch_size = batch_size
-
-#     def cache(self, state, next_state, action, reward, done):
-#         """
-#         Store the experience to self.memory (replay buffer)
-
-#         Inputs:
-#         state (LazyFrame),
-#         next_state (LazyFrame),
-#         action (int),
-#         reward (float),
-#         done(bool))
-#         """
-#         state = state.__array__()
-#         next_state = next_state.__array__()
-
-#         state = torch.tensor(state)
-#         next_state = torch.tensor(next_state)
-#         action = torch.tensor([action])
-#         reward = torch.tensor([reward])
-#         done = torch.tensor([done])
-
-#         self.memory.append((state, next_state, action, reward, done,))
-
-#     def sample(self):
-#         """
-#         Retrieve a batch of experiences from memory
-#         """
-#         batch = random.sample(self.memory, self.batch_size)
-#         state, next_state, action, reward, done = map(torch.stack, zip(*batch))
-#         return state, next_state, action.squeeze(), reward.squeeze(), done.squeeze()
-
-import random
-import numpy as np
+device = torch.device("cuda" if USE_CUDA else "cpu")
 
 class ReplayBuffer:
     def __init__(self, capacity, alpha=0.6):
@@ -50,17 +14,33 @@ class ReplayBuffer:
         self.priorities = np.zeros((capacity,), dtype=np.float32)
         self.position = 0
         self.alpha = alpha
-
+        
     def cache(self, state, next_state, action, reward, done):
         max_priority = self.priorities.max() if self.buffer else 1.0
-
+        
+        # Convert states to uint8 to save memory
+        if isinstance(state, np.ndarray):
+            state = np.asarray(state, dtype=np.uint8)
+        if isinstance(next_state, np.ndarray):
+            next_state = np.asarray(next_state, dtype=np.uint8)
+            
         if len(self.buffer) < self.capacity:
             self.buffer.append((state, next_state, action, reward, done))
         else:
+            # Free old tensors explicitly
+            old_state, old_next_state, _, _, _ = self.buffer[self.position]
+            del old_state
+            del old_next_state
             self.buffer[self.position] = (state, next_state, action, reward, done)
-
+            
         self.priorities[self.position] = max_priority
         self.position = (self.position + 1) % self.capacity
+        
+        # Periodic garbage collection
+        if self.position % 1000 == 0:
+            gc.collect()
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
 
     def sample(self, batch_size, beta=0.4):
         if len(self.buffer) == self.capacity:
@@ -80,11 +60,13 @@ class ReplayBuffer:
         weights = np.array(weights, dtype=np.float32)
 
         batch = list(zip(*samples))
-        states = np.array(batch[0])
-        next_states = np.array(batch[1])
-        actions = np.array(batch[2])
-        rewards = np.array(batch[3])
-        dones = np.array(batch[4])
+        
+        # Convert to tensors only when sampling
+        states = torch.from_numpy(np.array(batch[0], dtype=np.float32) / 255.0).to(device)
+        next_states = torch.from_numpy(np.array(batch[1], dtype=np.float32) / 255.0).to(device)
+        actions = torch.from_numpy(np.array(batch[2])).to(device)
+        rewards = torch.from_numpy(np.array(batch[3], dtype=np.float32)).to(device)
+        dones = torch.from_numpy(np.array(batch[4], dtype=np.float32)).to(device)
 
         return states, next_states, actions, rewards, dones, indices, weights
 
